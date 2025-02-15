896b49e19c0191bdb21d5ed21d90f1f1230b83d3
Sahil Prajapati <sahilprajapati882@gmail.com>
2025-02-15 16:15:48 +0530

    express files

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+//console.dir(app);
+
+let port = 3000;
+app.listen (port , () => {
+    console.log(`app is listening on port ${port}`);
+})
+
+// app.use((req, res) => {
+//     console.log("new incomming request");
+//    let code = "<h1>Fruits</h1> <ul><li>Apple</li> <li>Banana</li></ul>";
+//    res.send(code);
+// });
+
+app.get('/', (req, res) => {
+    res.send('GET request to homepage')
+  })
